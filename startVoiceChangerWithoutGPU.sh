27b6555aa88189c66710d2b808ebc8cbcbedba2a
wataru <wataru@fdev.local.com>
2022-08-26 02:38:01 +0900

    fix

--- startVoiceChangerWithoutGPU.sh
+++ startVoiceChangerWithoutGPU.sh
@@ -4,4 +4,4 @@ docker run -it --shm-size=128M \
   -v `pwd`/vc_resources:/resources \
   -e LOCAL_UID=$(id -u $USER) \
   -e LOCAL_GID=$(id -g $USER) \
-  -p 6006:6006 -p 8080:8080 dannadori/voice-changer:20220826_022939 "$@"
+  -p 6006:6006 -p 8080:8080 dannadori/voice-changer:20220826_023623 "$@"
