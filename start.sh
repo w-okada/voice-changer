ff4033dfbc6a8559832cd840ac2bc95cb669d2c6
wataru <wataru@fdev.local.com>
2022-08-26 09:29:11 +0900

    update

--- start.sh
+++ start.sh
@@ -10,4 +10,4 @@ docker run -it --gpus all --shm-size=128M \
   -v `pwd`/vc_resources:/resources \
   -e LOCAL_UID=$(id -u $USER) \
   -e LOCAL_GID=$(id -g $USER) \
-  -p 6006:6006 -p 8080:8080 dannadori/voice-changer:20220826_023623 "$@"
+  -p 6006:6006 -p 8080:8080 dannadori/voice-changer:20220826_092734 "$@"
