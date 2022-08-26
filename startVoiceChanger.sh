b278c85e6980de7481ab682207bbb3e6537a6c6f
wataru <wataru@fdev.local.com>
2022-08-26 09:41:17 +0900

    update

--- startVoiceChanger.sh
+++ startVoiceChanger.sh
@@ -4,4 +4,4 @@ docker run -it --gpus all --shm-size=128M \
   -v `pwd`/vc_resources:/resources \
   -e LOCAL_UID=$(id -u $USER) \
   -e LOCAL_GID=$(id -g $USER) \
-  -p 6006:6006 -p 8080:8080 dannadori/voice-changer:20220826_093634 "$@"
+  -p 6006:6006 -p 8080:8080 dannadori/voice-changer:20220826_093743 "$@"
