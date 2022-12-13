4520b3e7d7c016d9c16a6bc8223027d474f58784
wataru <wataru@fdev.local.com>
2022-12-13 22:33:14 +0900

    update

--- start2.sh
+++ start2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-DOCKER_IMAGE=dannadori/voice-changer:20221213_220109
+DOCKER_IMAGE=dannadori/voice-changer:20221213_223106
 # DOCKER_IMAGE=voice-changer
 
 if [ $# = 0 ]; then
