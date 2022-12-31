f6fc56678db73ac4d7fdee26d9b9c1bc43b90aa0
wataru <wataru@fdev.local.com>
2023-01-01 01:43:46 +0900

    restore log

--- server/misc/log_control.py
+++ server/misc/log_control.py
@@ -2,12 +2,12 @@ import logging
 
 # logging.getLogger('numba').setLevel(logging.WARNING)
 
-class UvicornSuppressFilter(logging.Filter):
-    def filter(self, record):
-        return False
+# class UvicornSuppressFilter(logging.Filter):
+#     def filter(self, record):
+#         return False
 
-logger = logging.getLogger("uvicorn.error")
-logger.addFilter(UvicornSuppressFilter())
+# logger = logging.getLogger("uvicorn.error")
+# logger.addFilter(UvicornSuppressFilter())
+# # logger.propagate = False
+# logger = logging.getLogger("multipart.multipart")
 # logger.propagate = False
-logger = logging.getLogger("multipart.multipart")
-logger.propagate = False
