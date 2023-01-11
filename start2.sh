30ddb95037e78a6857a21363c1b5bbcdc49db53c
wataru <wataru@fdev.local.com>
2023-01-11 18:14:52 +0900

    additional commit

--- start2.sh
+++ start2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eu
 
-# DOCKER_IMAGE=dannadori/voice-changer:20221214_053733
-DOCKER_IMAGE=voice-changer
+DOCKER_IMAGE=dannadori/voice-changer:20230111_180952
+# DOCKER_IMAGE=voice-changer
 
 if [ $# = 0 ]; then
     echo "
