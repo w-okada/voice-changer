558c812c1d67169aaca68200396b2492fe7f9421
wataru <wataru@fdev.local.com>
2023-01-12 23:08:28 +0900

    suppress uvicorn log

--- server/misc/log_control.py
+++ server/misc/log_control.py
@@ -2,13 +2,13 @@ import logging
 
 # logging.getLogger('numba').setLevel(logging.WARNING)
 
-# class UvicornSuppressFilter(logging.Filter):
-#     def filter(self, record):
-#         return False
+class UvicornSuppressFilter(logging.Filter):
+    def filter(self, record):
+        return False
 
-# logger = logging.getLogger("uvicorn.error")
-# logger.addFilter(UvicornSuppressFilter())
-# # logger.propagate = False
+logger = logging.getLogger("uvicorn.error")
+logger.addFilter(UvicornSuppressFilter())
+# logger.propagate = False
 logger = logging.getLogger("multipart.multipart")
 logger.propagate = False
 
