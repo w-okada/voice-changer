cd057a2fa3cb99890e3a1efd11bde5568cacaf23
wataru <wataru@fdev.local.com>
2023-01-16 05:31:49 +0900

    create folder in tmpdir. for mac app 2

--- server/const.py
+++ server/const.py
@@ -1,14 +1,15 @@
 import os, sys 
 import tempfile
 
-tmpdir = tempfile.TemporaryDirectory()
-print("generate tmpdir:::",tmpdir)
 frontend_path = os.path.join(sys._MEIPASS, "dist") if hasattr(sys, "_MEIPASS") else "../client/demo/dist"
 ERROR_NO_ONNX_SESSION = "ERROR_NO_ONNX_SESSION"
 
-SSL_KEY_DIR  = os.path.join(tmpdir, "keys") if hasattr(sys, "_MEIPASS") else "keys"
-MODEL_DIR = os.path.join(tmpdir, "logs") if hasattr(sys, "_MEIPASS") else "logs"
-UPLOAD_DIR = os.path.join(tmpdir, "upload_dir") if hasattr(sys, "_MEIPASS") else "upload_dir"
+
+tmpdir = tempfile.TemporaryDirectory()
+print("generate tmpdir:::",tmpdir)
+SSL_KEY_DIR  = os.path.join(tmpdir.name, "keys") if hasattr(sys, "_MEIPASS") else "keys"
+MODEL_DIR = os.path.join(tmpdir.name, "logs") if hasattr(sys, "_MEIPASS") else "logs"
+UPLOAD_DIR = os.path.join(tmpdir.name, "upload_dir") if hasattr(sys, "_MEIPASS") else "upload_dir"
 # SSL_KEY_DIR  = os.path.join(sys._MEIPASS, "keys") if hasattr(sys, "_MEIPASS") else "keys"
 # MODEL_DIR = os.path.join(sys._MEIPASS, "logs") if hasattr(sys, "_MEIPASS") else "logs"
 # UPLOAD_DIR = os.path.join(sys._MEIPASS, "upload_dir") if hasattr(sys, "_MEIPASS") else "upload_dir"
