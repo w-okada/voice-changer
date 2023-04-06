79abc3d66788078f3ef38bb378388b4415ffc5fb
wataru <wataru@fdev.local.com>
2023-04-07 05:26:42 +0900

    WIP: gui commonize 2

--- client/buildAllDemo.sh
+++ client/buildAllDemo.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-cd demo_v13 && ncu -u && npm install && npm run build:prod && cd -
-cd demo_v15 && ncu -u && npm install && npm run build:prod && cd -
-cd demo_so-vits-svc_40 && ncu -u && npm install && npm run build:prod && cd -
-cd demo_so-vits-svc_40_c && ncu -u && npm install && npm run build:prod && cd -
-cd demo_so-vits-svc_40v2 && ncu -u && npm install && npm run build:prod && cd -
-cd demo_so-vits-svc_40v2_c && ncu -u && npm install && npm run build:prod && cd -
-cd demo_DDSP-SVC && ncu -u && npm install && npm run build:prod && cd -
-
+cd demo && ncu -u && npm install && npm run build:prod && cd -
