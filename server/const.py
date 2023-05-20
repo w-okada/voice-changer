d62a9fffd41a7df2a2a051a57e92721cd5dd1bda
bugyo <bugyo@bugyo.tk>
2023-05-20 16:33:17 +0900

    Added CREPE f0 detection

--- server/const.py
+++ server/const.py
@@ -85,6 +85,7 @@ class EnumInferenceTypes(Enum):
 class EnumPitchExtractorTypes(Enum):
     harvest = "harvest"
     dio = "dio"
+    crepe = "crepe"
 
 
 class EnumFrameworkTypes(Enum):
--- server/voice_changer/RVC/pitchExtractor/PitchExtractorManager.py
+++ server/voice_changer/RVC/pitchExtractor/PitchExtractorManager.py
@@ -2,6 +2,7 @@ from typing import Protocol
 from const import EnumPitchExtractorTypes
 from voice_changer.RVC.pitchExtractor.DioPitchExtractor import DioPitchExtractor
 from voice_changer.RVC.pitchExtractor.HarvestPitchExtractor import HarvestPitchExtractor
+from voice_changer.RVC.pitchExtractor.CrepePitchExtractor import CrepePitchExtractor
 from voice_changer.RVC.pitchExtractor.PitchExtractor import PitchExtractor
 
 
@@ -29,6 +30,11 @@ class PitchExtractorManager(Protocol):
             or pitchExtractorType == EnumPitchExtractorTypes.dio.value
         ):
             return DioPitchExtractor()
+        elif (
+            pitchExtractorType == EnumPitchExtractorTypes.crepe
+            or pitchExtractorType == EnumPitchExtractorTypes.crepe.value
+        ):
+            return CrepePitchExtractor()
         else:
             # return hubert as default
             raise RuntimeError(
