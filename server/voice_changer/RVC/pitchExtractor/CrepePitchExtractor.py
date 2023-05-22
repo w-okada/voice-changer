1643628a6905520e59a3b1aaa9815afb5469f486
wataru <wataru@fdev.local.com>
2023-05-22 14:43:25 +0900

    crepe gui

--- server/voice_changer/RVC/pitchExtractor/CrepePitchExtractor.py
+++ server/voice_changer/RVC/pitchExtractor/CrepePitchExtractor.py
@@ -9,10 +9,10 @@ class CrepePitchExtractor(PitchExtractor):
     def __init__(self):
         super().__init__()
         if torch.cuda.is_available():
-            self.device = torch.device('cuda:' + str(torch.cuda.current_device()))
+            self.device = torch.device("cuda:" + str(torch.cuda.current_device()))
         else:
-            self.device = torch.device('cpu')
-        
+            self.device = torch.device("cpu")
+
     def extract(self, audio, f0_up_key, sr, window, silence_front=0):
         n_frames = int(len(audio) // window) + 1
         start_frame = int(silence_front * sr / window)
@@ -26,12 +26,23 @@ class CrepePitchExtractor(PitchExtractor):
         f0_mel_min = 1127 * np.log(1 + f0_min / 700)
         f0_mel_max = 1127 * np.log(1 + f0_max / 700)
 
-        f0 = torchcrepe.predict(torch.tensor(audio).unsqueeze(0), sr,
-                hop_length=window, fmin=f0_min, fmax=f0_max, model='tiny', batch_size=256,
-                decoder=torchcrepe.decode.weighted_argmax, device=self.device)
+        f0 = torchcrepe.predict(
+            torch.tensor(audio).unsqueeze(0),
+            sr,
+            hop_length=window,
+            fmin=f0_min,
+            fmax=f0_max,
+            # model="tiny",
+            model="full",
+            batch_size=256,
+            decoder=torchcrepe.decode.weighted_argmax,
+            device=self.device,
+        )
         f0 = f0.squeeze().detach().cpu().numpy()
 
-        f0 = np.pad(f0.astype("float"), (start_frame, n_frames - f0.shape[0] - start_frame))
+        f0 = np.pad(
+            f0.astype("float"), (start_frame, n_frames - f0.shape[0] - start_frame)
+        )
 
         f0 *= pow(2, f0_up_key / 12)
         f0bak = f0.copy()
@@ -44,4 +55,3 @@ class CrepePitchExtractor(PitchExtractor):
         f0_coarse = np.rint(f0_mel).astype(np.int)
 
         return f0_coarse, f0bak
-
