ae3587a6157a78c8c5b37ea5f1591318869e5791
w-okada <wataru@gal2>
2023-07-10 03:18:51 +0900

    update docker

--- start_docker.sh
+++ start_docker.sh
@@ -27,11 +27,16 @@ if [ "${USE_GPU}" = "on" ]; then
     -p ${EX_PORT}:18888 \
     $DOCKER_IMAGE -p 18888 --https true \
         --content_vec_500 pretrain/checkpoint_best_legacy_500.pt  \
+        --content_vec_500_onnx pretrain/content_vec_500.onnx \
+        --content_vec_500_onnx_on true \
         --hubert_base pretrain/hubert_base.pt \
+        --hubert_base_jp pretrain/rinna_hubert_base_jp.pt \
         --hubert_soft pretrain/hubert/hubert-soft-0d54a1f4.pt \
         --nsf_hifigan pretrain/nsf_hifigan/model \
-        --hubert_base_jp pretrain/rinna_hubert_base_jp.pt \
-        --model_dir model_dir
+        --crepe_onnx_full pretrain/crepe_onnx_full.onnx \
+        --crepe_onnx_tiny pretrain/crepe_onnx_tiny.onnx \
+        --model_dir model_dir \
+        --samples samples.json
 else
     echo "VC Client start...(cpu)"
     docker run -it --rm --shm-size=1024M \
@@ -42,11 +47,16 @@ else
     -p ${EX_PORT}:18888 \
     $DOCKER_IMAGE -p 18888 --https true \
         --content_vec_500 pretrain/checkpoint_best_legacy_500.pt  \
+        --content_vec_500_onnx pretrain/content_vec_500.onnx \
+        --content_vec_500_onnx_on true \
         --hubert_base pretrain/hubert_base.pt \
+        --hubert_base_jp pretrain/rinna_hubert_base_jp.pt \
         --hubert_soft pretrain/hubert/hubert-soft-0d54a1f4.pt \
         --nsf_hifigan pretrain/nsf_hifigan/model \
-        --hubert_base_jp pretrain/rinna_hubert_base_jp.pt \
-        --model_dir model_dir
+        --crepe_onnx_full pretrain/crepe_onnx_full.onnx \
+        --crepe_onnx_tiny pretrain/crepe_onnx_tiny.onnx \
+        --model_dir model_dir \
+        --samples samples.json
 fi
 
 
